895e864f6ce087477d241a4002b185740db82bf8
AnaneySinghal <ananey.singhal_cs23@gla.ac.in>
2026-03-18 16:51:08 +0530

    18-03-26

--- project/routes/userRouts.js
+++ project/routes/userRouts.js
@@ -2,10 +2,18 @@ const express =require("express")
 
 const router =express.Router();
 
-const userController =require("../controllers/user")
+// const userController =require("../controllers/user")
 
-route.get("/users",userController.getUsers)
-route.get("/user/:id",userController.getUsersById)
-route.post("/add/users",userController.addUsers)
+// route.get("/users",userController.getUsers)
+
+// route.get("/user/:id",userController.getUsersById)
+// route.post("/add/users",userController.addUsers)
+
+router.get("/users",userController.getUsers);
+router.post("/users",userController.addUsers);
+
+router.route("/users").get(userController.getUsers).post(userController.addUsers);
+
+router.get("/users/:id",userController.getUsersById);
 
 module.exports =router
